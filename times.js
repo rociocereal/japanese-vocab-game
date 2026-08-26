e3f3a1448ee4148bfc2c801c5047dabcfcbfb285
rociocereal <rociocereal0567@gmail.com>
2026-08-26 22:34:51 +0800

    新增時間單字

--- times.js
+++ times.js
@@ -1,26 +1,193 @@
 const times = [
+    //年
+    {
+        chinese: "前年",
+        answers: ["おととし"],
+        tense: "past"
+    },
+    {
+        chinese: "去年",
+        answers: ["去年", "きょねん"],
+        tense: "past"
+    },
+    {
+        chinese: "今年",
+        answers: ["今年", "ことし"],
+        tense: "nonPast"
+    },
+    {
+        chinese: "明年",
+        answers: ["来年", "らいねん"],
+        tense: "nonPast"
+    },
+    {   
+        chinese: "後年",   
+        answers: ["さ来年", "さらいねん"],
+        tense: "nonPast"
+    },
+    {
+        chinese: "每年",
+        answers: ["毎年", "まいとし"],
+        tense: "nonPast"
+    },
+
+    //月
+    {
+        chinese: "上上個月",
+        answers: ["先々月", "せんせんげつ"], 
+        tense: "past"
+    },
+    {
+        chinese: "上個月",
+        answers: ["先月", "せんげつ"],
+        tense: "past"
+    },
+    {
+        chinese: "本月",
+        answers: ["今月", "こんげつ"],
+        tense: "nonPast"
+    },
+    {
+        chinese: "下個月",
+        answers: ["来月", "らいげつ"],
+        tense: "nonPast"
+    },
+    {   
+        chinese: "下下個月",   
+        answers: ["さ来月", "さらいげつ"],
+        tense: "nonPast"
+    },
+    {
+        chinese: "每月",
+        answers: ["毎月", "まいつき"],
+        tense: "nonPast"
+    },
+
+    //週
+    {
+        chinese: "上上週",
+        answers: ["先々週", "せんせんしゅう"], 
+        tense: "past"
+    },
+    {
+        chinese: "上週",
+        answers: ["先週", "せんしゅう"],
+        tense: "past"
+    },
+    {
+        chinese: "本週",
+        answers: ["今週", "こんしゅう"],
+        tense: "nonPast"
+    },
+    {
+        chinese: "下週",
+        answers: ["来週", "らいしゅう"],
+        tense: "nonPast"
+    },
+    {   
+        chinese: "下下週",   
+        answers: ["さ来週", "さらいしゅう"],
+        tense: "nonPast"
+    },
+    {
+        chinese: "每週",
+        answers: ["毎週", "まいしゅう"],
+        tense: "nonPast"
+    },
+
+    //日
     {
         chinese: "前天",
-        answers: ["おととい"] 
+        answers: ["おととい"], 
+        tense: "past"
     },
     {
         chinese: "昨天",
-        answers: ["昨日", "きのう"]
+        answers: ["昨日", "きのう"],
+        tense: "past"
     },
     {
         chinese: "今天",
-        answers: ["今日", "きょう"]
+        answers: ["今日", "きょう"],
+        tense: "nonPast"
     },
     {
         chinese: "明天",
-        answers: ["明日", "あした"]
+        answers: ["あした"],
+        tense: "nonPast"
     },
     {   
         chinese: "後天",   
-        answers: ["あさって"]
+        answers: ["あさって"],
+        tense: "nonPast"
     },
     {
         chinese: "每天",
-        answers: ["毎日", "まいにち"]
+        answers: ["毎日", "まいにち"],
+        tense: "nonPast"
+    },
+
+    //早上
+    {
+        chinese: "前天早上",
+        answers: ["おとといの朝", "おとといのあさ"],
+        tense: "past"
+    },
+    {
+        chinese: "昨天早上",
+        answers: ["昨日の朝", "きのうのあさ"],
+        tense: "past"
+    },
+    {
+        chinese: "今天早上",
+        answers: ["今朝", "けさ"],
+        tense: "past"
+    },
+    {
+        chinese: "明天早上",
+        answers: ["あしたの朝", "あしたのあさ"],
+        tense: "nonPast"
+    },
+    {   
+        chinese: "後天早上",   
+        answers: ["あさっての朝", "あさってのあさ"],
+        tense: "nonPast"
+    },
+    {
+        chinese: "每天早上",
+        answers: ["毎朝", "まいあさ"],
+        tense: "nonPast"
+    }, 
+
+    //晚上
+    {
+        chinese: "前天晚上",
+        answers: ["おとといの晩", "おとといのばん"],
+        tense: "past"
+    },
+    {
+        chinese: "昨天晚上",
+        answers: ["ゆうべ"],
+        tense: "past"
+    },
+    {
+        chinese: "今天晚上",
+        answers: ["今晩", "こんばん"],
+        tense: "nonPast"
+    },
+    {
+        chinese: "明天晚上",
+        answers: ["あしたの晩", "あしたのばん"],
+        tense: "nonPast"
+    },
+    {   
+        chinese: "後天晚上",
+        answers: ["あさっての晩", "あさってのばん"],
+        tense: "nonPast"
+    },
+    {
+        chinese: "每天晚上",
+        answers: ["毎晩", "まいばん"],
+        tense: "nonPast"
     }
 ];
