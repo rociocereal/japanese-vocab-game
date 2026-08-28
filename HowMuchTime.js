c7dd231b9214cef2b723c9eecb4bb18c3536d538
rociocereal <rociocereal0567@gmail.com>
2026-08-28 15:54:06 +0800

    新增多少時間的單字

---
+++ HowMuchTime.js
@@ -0,0 +1,334 @@
+const minutes = [
+    //幾分鐘
+    {
+        chinese: "幾分鐘",
+        answers: ["何分", "なんぷん"]
+    },
+    {
+        chinese: "一分鐘",
+        answers: ["いっぷん"]
+    },
+    {
+        chinese: "兩分鐘",
+        answers: ["にふん"]
+    },
+    {
+        chinese: "三分鐘",
+        answers: ["さんぷん"]
+    },
+    {
+        chinese: "四分鐘",
+        answers: ["よんぷん"]
+    },
+    {
+        chinese: "五分鐘",
+        answers: ["ごふん"]
+    }, 
+    {
+        chinese: "六分鐘",
+        answers: ["ろっぷん"]
+    },
+    {
+        chinese: "七分鐘",
+        answers: ["ななふん"]
+    },
+    {
+        chinese: "八分鐘",
+        answers: ["はっぷん"]
+    },
+    {
+        chinese: "九分鐘",
+        answers: ["きゅうふん"]
+    },
+    {
+        chinese: "十分鐘",
+        answers: ["じゅっぷん"]
+    },
+    {
+        chinese: "十一分鐘",
+        answers: ["じゅういっぷん"]
+    },
+    {
+        chinese: "十二分鐘",
+        answers: ["じゅうにふん"]
+    }
+]
+
+const hours = [
+    //幾小時
+    {
+        chinese: "幾小時",
+        answers: ["何時間", "なんじかん"]
+    },
+    { 
+        chinese: "一小時",
+        answers: ["いちじかん"]
+    },
+    {
+        chinese: "兩小時",
+        answers: ["にじかん"]
+    },
+    {
+        chinese: "三小時",
+        answers: ["さんじかん"]
+    },
+    {
+        chinese: "四小時",
+        answers: ["よじかん"]
+    },
+    {
+        chinese: "五小時",
+        answers: ["ごじかん"]
+    },
+    {
+        chinese: "六小時",
+        answers: ["ろくじかん"]
+    },
+    {
+        chinese: "七小時",
+        answers: ["しちじかん"]
+    },
+    {
+        chinese: "八小時",
+        answers: ["はちじかん"]
+    },
+    {
+        chinese: "九小時",
+        answers: ["くじかん"]
+    },
+    {
+        chinese: "十小時",
+        answers: ["じゅうじかん"]
+    },
+    {
+        chinese: "十一小時",
+        answers: ["じゅういちじかん"]
+    },
+    {
+        chinese: "十二小時",
+        answers: ["じゅうにじかん"]
+    }
+]
+
+const days = [
+    //幾天
+    {
+        chinese: "幾天",
+        answers: ["何日", "なんにち"]
+    },
+    {
+        chinese: "一天",
+        answers: ["いちにち"]
+    },
+    {
+        chinese: "兩天",
+        answers: ["ふつか"]
+    },
+    {
+        chinese: "三天",
+        answers: ["みっか"]
+    },
+    {
+        chinese: "四天",
+        answers: ["よっか"]
+    },
+    {
+        chinese: "五天",
+        answers: ["いつか"]
+    },
+    {
+        chinese: "六天",
+        answers: ["むいか"]
+    },
+    {
+        chinese: "七天",
+        answers: ["なのか"]
+    },
+    {
+        chinese: "八天",
+        answers: ["ようか"]
+    },
+    {
+        chinese: "九天",
+        answers: ["ここのか"]
+    },
+    {
+        chinese: "十天",
+        answers: ["とおか"]
+    },
+    {
+        chinese: "十一天",
+        answers: ["じゅういちにち"]
+    },
+    {
+        chinese: "十二天",
+        answers: ["じゅうににち"]
+    }
+]
+
+const weeks = [
+    //幾週
+    {
+        chinese: "幾週",
+        answers: ["何週間", "なんしゅうかん"]
+    },
+    {
+        chinese: "一週",
+        answers: ["いっしゅうかん"]
+    },
+    {
+        chinese: "兩週",
+        answers: ["にしゅうかん"]
+    },
+    {
+        chinese: "三週",
+        answers: ["さんしゅうかん"]
+    },
+    {
+        chinese: "四週",
+        answers: ["よんしゅうかん"]
+    },
+    {
+        chinese: "五週",
+        answers: ["ごしゅうかん"]
+    },
+    {
+        chinese: "六週",
+        answers: ["ろくしゅうかん"]
+    },
+    {
+        chinese: "七週",
+        answers: ["ななしゅうかん"]
+    },
+    {
+        chinese: "八週",
+        answers: ["はっしゅうかん"]
+    },
+    {
+        chinese: "九週",
+        answers: ["きゅうしゅうかん"]
+    },
+    {
+        chinese: "十週",
+        answers: ["じゅうしゅうかん"]
+    },
+    {
+        chinese: "十一週",
+        answers: ["じゅういっしゅうかん"]
+    },
+    {
+        chinese: "十二週",
+        answers: ["じゅうにしゅうかん"]
+    }
+]
+
+const months = [
+    //幾個月
+    {
+        chinese: "幾個月",
+        answers: ["何か月", "なんかげつ"]
+    },
+    {
+        chinese: "一個月",
+        answers: ["いっかげつ"]
+    },
+    {
+        chinese: "兩個月",
+        answers: ["にかげつ"]
+    },
+    {
+        chinese: "三個月",
+        answers: ["さんかげつ"]
+    },
+    {
+        chinese: "四個月",
+        answers: ["よんかげつ"]
+    },
+    {
+        chinese: "五個月",
+        answers: ["ごかげつ"]
+    },
+    {
+        chinese: "六個月",
+        answers: ["ろっかげつ"]
+    },
+    {
+        chinese: "七個月",
+        answers: ["ななかげつ"]
+    },
+    {
+        chinese: "八個月",
+        answers: ["はちかげつ"]
+    },
+    {
+        chinese: "九個月",
+        answers: ["きゅうかげつ"]
+    },
+    {
+        chinese: "十個月",
+        answers: ["じゅっかげつ"]
+    },
+    {
+        chinese: "十一個月",
+        answers: ["じゅういっかげつ"]
+    },
+    {
+        chinese: "十二個月",
+        answers: ["じゅうにかげつ"]
+    }
+]
+
+const years = [
+    //幾年
+    {
+        chinese: "幾年",
+        answers: ["何年", "なんねん"]
+    },
+    {
+        chinese: "一年",
+        answers: ["いちねん"]
+    },
+    {
+        chinese: "兩年",
+        answers: ["にねん"]
+    },
+    {
+        chinese: "三年",
+        answers: ["さんねん"]
+    },
+    {
+        chinese: "四年",
+        answers: ["よねん"]
+    },
+    {
+        chinese: "五年",
+        answers: ["ごねん"]
+    },
+    {   chinese: "六年",
+        answers: ["ろくねん"]
+    },
+    {
+        chinese: "七年",
+        answers: ["ななねん"]
+    },
+    {
+        chinese: "八年",
+        answers: ["はちねん"]
+    },
+    {
+        chinese: "九年",
+        answers: ["きゅうねん"]
+    },
+    {
+        chinese: "十年",
+        answers: ["じゅうねん"]
+    },
+    {
+        chinese: "十一年",
+        answers: ["じゅういちねん"]
+    },
+    {
+        chinese: "十二年",
+        answers: ["じゅうにねん"]
+    }
+]
